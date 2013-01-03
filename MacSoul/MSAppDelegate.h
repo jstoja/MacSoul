e6b48f58d6c50b89d1662ac55cd555d7652eac8e
Julien Bordellier <julienbordellier@gmail.com>
2013-01-03 19:46:39 +0100

    Initial Commit

---
+++ MacSoul/MSAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MSAppDelegate.h
+//  MacSoul
+//
+//  Created by Julien Bordellier on 03/01/13.
+//  Copyright (c) 2013 Epitech. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface MSAppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ MacSoul/MSAppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  MSAppDelegate.m
+//  MacSoul
+//
+//  Created by Julien Bordellier on 03/01/13.
+//  Copyright (c) 2013 Epitech. All rights reserved.
+//
+
+#import "MSAppDelegate.h"
+
+@implementation MSAppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
